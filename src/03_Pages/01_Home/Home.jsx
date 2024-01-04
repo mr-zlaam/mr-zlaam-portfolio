9fd8e9a8731cf471fe9a5471090ea8099dd75d7c
Zlaam <mrzalaam@gmail.com>
2024-01-04 21:50:37 +0500

    last stable home page

--- src/03_Pages/01_Home/Home.jsx
+++ src/03_Pages/01_Home/Home.jsx
@@ -1,9 +1,23 @@
 import {} from "react";
-
+import "./Home.scss";
 const Home = () => {
   return (
     <>
-      <div>Home</div>
+      <div className="home_parent">
+        <div className="home_container">
+          <div className="home_content">
+            <div className="home_about">
+              Hi. {"I'm"} Zláam. <br />A FullStack Web-Dev.
+            </div>
+            <div className="about_img_container ">
+              <img
+                src="https://live.staticflickr.com/65535/53281434521_eeef635514_z.jpg"
+                alt="zlaam"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
