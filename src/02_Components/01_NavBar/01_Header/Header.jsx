db185040d5a114653ed5eb8c3e6c30d7007a6504
Zlaam <mrzalaam@gmail.com>
2024-01-04 15:04:35 +0500

    last stable commit for variables

--- src/02_Components/01_NavBar/01_Header/Header.jsx
+++ src/02_Components/01_NavBar/01_Header/Header.jsx
@@ -1,9 +1,14 @@
 import {} from "react";
 import "./Header.scss";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <div className="nav">Navbar</div>
+      <div className="nav">
+        <span className="cta">
+          <Link className="link_animation_dark"> Shop now </Link>
+        </span>
+      </div>
     </>
   );
 };
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Parents from "./00_Parents/Parent";
+import Cursor from "./02_Components/Cursor/Cursor";
 
 function App() {
   return (
     <>
+      <Cursor />
       <Parents />
     </>
   );
