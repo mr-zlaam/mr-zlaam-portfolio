5257ed14f8e78afadf3aba9acf456f583440dbd7
Zlaam <mrzalaam@gmail.com>
2024-01-07 11:22:34 +0500

    last stable commit

--- src/04_Variables_And_StyleComponents/Project_Modal/Project_Modal.jsx
+++ src/04_Variables_And_StyleComponents/Project_Modal/Project_Modal.jsx
@@ -17,10 +17,16 @@ const Project_Modal = () => {
         }`}
       >
         <div onClick={handle_modal_closer} className="modal_closer">
-          <FaArrowLeft className="modal_closer_icon" />
+          <FaArrowLeft />
         </div>
-        <img src={isSelectedData.image} alt="" />
-        <h1>{isSelectedData.project_title}</h1>
+        <div className="image_controller">
+          <img src={isSelectedData.image} alt="" />
+        </div>
+        <div className="description_controller">
+          <h1 className="project_title">{isSelectedData.project_title}</h1>
+          <h1 className="project_desc_txt">About Project:-</h1>
+          <p className="project_desc">{isSelectedData.project_desc}</p>
+        </div>
       </div>
     </>
   );
