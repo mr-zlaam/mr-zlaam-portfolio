3608407302a9a203323d93ca04ad863c3893cc58
Zlaam <mrzalaam@gmail.com>
2024-05-19 19:05:36 +0500

    update AppLayout.jsx

--- src/05_Layouts/AppLayout.jsx
+++ src/05_Layouts/AppLayout.jsx
@@ -3,6 +3,8 @@ import Header from "../02_Components/01_NavBar/01_Header/Header";
 import { Outlet } from "react-router-dom";
 
 function AppLayout() {
+  alert("This site is no longer maintained.");
+
   return (
     <>
       <Header />
